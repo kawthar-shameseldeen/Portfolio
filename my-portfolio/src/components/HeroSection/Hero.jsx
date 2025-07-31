116e2a7127b86847247789e67cbb7f92438c4b84
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2025-07-31 20:35:26 +0300

    Refactor App layout and add HeroSection component. Updated App.css for a responsive design with a yellow background and centered white container. Introduced HeroSection component with basic styling.

---
+++ my-portfolio/src/components/HeroSection/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css';
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+        <div className="hero-container">
+           
+            
+        </div>
+    </section>
+  );
+};
+
+export default HeroSection;
